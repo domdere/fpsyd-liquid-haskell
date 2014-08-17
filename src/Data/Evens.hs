5730e5b53d9082c7b3e54d83dc9795fde0b4aeec
Dom De Re <domdere@domdere.com>
2014-08-17 15:52:30 +1000

    Added sample module with liquid type annotations

---
+++ src/Data/Evens.hs
@@ -0,0 +1,49 @@
+-------------------------------------------------------------------
+-- |
+-- Module       : Data.Evens
+-- Copyright    : (C) 2014
+-- License      : BSD-style (see the file etc/LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-- The "Basics > Even Numbers" demo in the Liquid Demo
+--
+-- <http://goto.ucsd.edu:8090/index.html>
+--
+-------------------------------------------------------------------
+module Data.Evens where
+
+import LocalPrelude
+
+{-@ type Even = {v:Int | v mod 2 = 0} @-}
+
+{-@ weAreEven :: [Even] @-}
+weAreEven :: [Int]
+weAreEven = [negate 10, negate 4, 0, 2, 666]
+
+{-@ notEven :: Even @-}
+notEven :: Int
+notEven = 7
+
+{-@ isEven :: n:Nat -> {v:Bool | ((Prop v) <=> (n mod 2 = 0))} @-}   
+isEven   :: Int -> Bool 
+isEven 0 = True
+isEven 1 = False
+isEven n = not (isEven (n-1))
+
+{-@ evens :: n:Nat -> [Even] @-}
+evens :: Int -> [Int]
+evens n = [i | i <- range 0 n, isEven i]
+
+{-@ range :: lo:Int -> hi:Int -> [{v:Int | (lo <= v && v < hi)}] / [hi -lo] @-}
+range :: Int -> Int -> [Int]
+range lo hi 
+  | lo < hi   = lo : range (lo+1) hi
+  | otherwise = []
+{-@ shift :: [Even] -> Even -> [Even] @-}
+shift :: [Int] -> Int -> [Int]
+shift xs k = [x + k | x <- xs]
+
+{-@ double :: [Nat] -> [Even] @-}
+double :: [Int] -> [Int]
+double xs = [x + x | x <- xs]
+
---
+++ src/LocalPrelude.hs
@@ -0,0 +1,23 @@
+-------------------------------------------------------------------
+-- |
+-- Module       : LocalPrelude
+-- Copyright    : (C) 2014
+-- License      : BSD-style (see the file etc/LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-- Prelude for this project.
+--
+-------------------------------------------------------------------
+module LocalPrelude (
+    -- * Type Classes
+        Num(..)
+    ,   Ord(..)
+    -- * Types
+    ,   Bool(..)
+    ,   Int
+    -- * Functions
+    ,   not
+    ,   otherwise
+    ) where
+
+import Prelude
